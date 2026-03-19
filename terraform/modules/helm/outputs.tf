11634e613871c1a8988cda11f329c0c607aadbbb
khsik6163 <jordan6163@naver.com>
2026-03-19 18:51:21 +0900

    feat(aiops): AMP workspace + IRSA 추가, Helm 릴리즈 ArgoCD로 이관

--- terraform/modules/helm/outputs.tf
+++ terraform/modules/helm/outputs.tf
@@ -12,3 +12,23 @@ output "cluster_autoscaler_role_arn" {
   description = "ARN of the Cluster Autoscaler IAM role"
   value       = aws_iam_role.cluster_autoscaler.arn
 }
+
+output "amp_workspace_id" {
+  description = "AMP workspace ID"
+  value       = aws_prometheus_workspace.main.id
+}
+
+output "amp_endpoint" {
+  description = "AMP workspace endpoint"
+  value       = aws_prometheus_workspace.main.prometheus_endpoint
+}
+
+output "prometheus_amp_role_arn" {
+  description = "ARN of the Prometheus IRSA role for AMP remote_write"
+  value       = aws_iam_role.prometheus_amp.arn
+}
+
+output "grafana_amp_role_arn" {
+  description = "ARN of the Grafana IRSA role for AMP query"
+  value       = aws_iam_role.grafana_amp.arn
+}
